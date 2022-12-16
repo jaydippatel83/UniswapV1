7bffe273197eca140ed51b91ff32777b25f15f68
Jaydippatel <macbook@MacBooks-MacBook-Pro-2.local>
2022-12-16 11:30:15 +0530

    add mumbai chain

--- dexchange/pages/index.js
+++ dexchange/pages/index.js
@@ -293,9 +293,9 @@ export default function Home() {
 
     // If user is not connected to the Goerli network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
+    if (chainId !== 80001) {
+      window.alert("Change the network to Mumbai");
+      throw new Error("Change network to Mumbai");
     }
 
     if (needSigner) {
@@ -314,7 +314,7 @@ export default function Home() {
       // Assign the Web3Modal class to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
-        network: "goerli",
+        network: "mumbai",
         providerOptions: {},
         disableInjectedProvider: false,
       });
